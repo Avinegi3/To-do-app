14cddc52e9856e3ca624cf715522f0210183bff9
Avi Negi <68230390+Avinegi3@users.noreply.github.com>
2022-01-06 22:32:47 +0530

    Add files via upload

---
+++ date.js
@@ -0,0 +1,16 @@
+//jshint esversion:6
+exports.getDate= function (){
+  const today = new Date();
+
+options= {
+  weekday: "long",
+  day: "numeric",
+  month: "long"
+};
+
+const day = today.toLocaleDateString("en-US",options);
+
+return day;
+};
+
+
